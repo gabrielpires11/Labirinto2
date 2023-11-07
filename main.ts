1892350ad0311ac32481f451ab6eed106efea16a
gabrielpires11 <gabri4590@gmail.com>
2023-11-07 17:03:58 -0300

    Gabriel

--- main.ts
+++ main.ts
@@ -1,1 +1,113 @@
-console.log("Olá Mundo");
+const color = require("cli-color");
+const input = require("prompt-sync")();
+const planta: number[][] = [
+    [22, 22, 22, 22, 22, 33, 11, 22, 22, 22, 22, 22, 22, 11, 11, 22, 22, 22, 22, 22],
+    [22, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 22],
+    [22, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 22],
+    [22, 11, 88, 22, 22, 11, 11, 22, 22, 11, 11, 22, 22, 11, 11, 22, 22, 88, 11, 22],
+    [22, 11, 11, 22, 22, 11, 11, 22, 22, 88, 11, 22, 22, 11, 11, 22, 22, 11, 11, 22],
+    [22, 11, 11, 22, 22, 11, 11, 22, 22, 11, 11, 22, 22, 11, 11, 22, 22, 88, 11, 22],
+    [22, 11, 11, 22, 22, 88, 11, 22, 22, 11, 11, 22, 22, 11, 11, 22, 22, 11, 11, 22],
+    [22, 11, 11, 22, 22, 11, 11, 22, 22, 11, 11, 22, 22, 11, 11, 22, 22, 11, 11, 22],
+    [22, 11, 11, 22, 22, 11, 11, 22, 22, 11, 11, 22, 22, 11, 11, 22, 22, 11, 11, 22],
+    [22, 11, 11, 22, 22, 11, 11, 22, 22, 11, 11, 22, 22, 11, 11, 22, 22, 11, 11, 22],
+    [22, 11, 11, 22, 22, 11, 11, 22, 22, 11, 11, 22, 22, 11, 11, 22, 22, 11, 11, 22],
+    [22, 11, 11, 22, 22, 11, 11, 22, 22, 11, 11, 22, 22, 11, 11, 22, 22, 11, 11, 22],
+    [22, 11, 11, 22, 22, 11, 11, 22, 22, 11, 11, 22, 22, 11, 11, 22, 22, 11, 11, 22],
+    [22, 11, 11, 22, 22, 11, 11, 22, 22, 11, 11, 22, 22, 11, 11, 22, 22, 11, 11, 22],
+    [22, 11, 11, 88, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 22],
+    [22, 11, 11, 11, 88, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 22],
+    [22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22],
+    [22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22]
+]
+
+const colorir = (text:number): string => {
+    if (text == 22) {
+        return color.red.bgRed(text)
+    } else if (text == 11) {
+        return color.blue.bgBlue(text)
+    } else if (text == 33) {
+        return color.green.bgGreen(text)
+    } else {
+        return color.yellow.bgYellow(text)
+    }
+}
+
+var x: number = 0;
+var y: number = 5;
+var passos: number = 0;
+
+const mostrarPlanta = (): void => {
+    console.clear();
+    for (let i of planta) {
+        let linha:any[] = [];
+        for (let e of i) {
+            linha.push(colorir(e))
+        }
+        console.log(linha.toString().replaceAll(',',''))
+    }
+    console.log("Passos", passos);
+}
+
+const descer = (): void => {
+    if (x + 1 <= planta.length && planta[x + 1][y] !== 22) {
+    planta [x][y] = 11;
+    x = x + 1;
+    planta [x][y] = 33;
+    passos++;
+    }
+}
+
+const subir = (): void => {
+    if (x - 1 >= 0 && planta[x - 1][y] !== 22) {
+    planta [x][y]= 11;
+    x = x - 1;
+    planta [x][y] = 33;
+    passos++;
+    }
+}  
+
+const esquerda = (): void => {
+    if (y - 1 >= 0 && planta[x][y - 1] !== 22) {
+    planta [x][y]= 11;
+    y = y - 1;
+    planta [x][y] = 33;
+    passos++;
+    }
+}
+
+const direita = (): void => {
+    if (y + 1 < planta[0].length && planta[x][y + 1] !== 22) {
+    planta [x][y]= 11;
+    y = y + 1;
+    planta [x][y] = 33;
+    passos++;
+    }
+}
+
+const verificarProduto = ():boolean => {
+    for (var linha of planta) {
+        for (var item of linha) {
+            if (item == 88) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+while (true) {
+    mostrarPlanta();
+    let escolha:string = input("Digite uma direção (w, a, s, d) ou (q) para sair: ")
+    if (escolha.toUpperCase() == "Q" || verificarProduto()) {
+        break
+    } else if (escolha.toUpperCase() == "W") {
+        subir();
+    } else if (escolha.toUpperCase() == "A") {
+        esquerda();
+    } else if (escolha.toUpperCase() == "S") {
+        descer();
+    } else if (escolha.toUpperCase() == "D") {
+        direita();
+    }
+}
